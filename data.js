5f0a057ef175ed5f3aba95e5c79035932ae2acdc
DamianCzy <156086971+DamianCzy@users.noreply.github.com>
2024-01-09 18:00:32 +0100

    Upload

---
+++ data.js
@@ -0,0 +1,181 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-lfns_salon_1",
+      "name": "LFNS_SALON_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.21612157034741664,
+        "pitch": 0.02247843466188293,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4667562507345373,
+          "pitch": 0.12042680088633873,
+          "rotation": 1.5707963267948966,
+          "target": "1-lfns_salon_2"
+        },
+        {
+          "yaw": -0.174687998016676,
+          "pitch": 0.12922350272900474,
+          "rotation": 1.5707963267948966,
+          "target": "3-lfns_salon_4"
+        },
+        {
+          "yaw": 0.22501503939874112,
+          "pitch": 0.0690740415658393,
+          "rotation": 0,
+          "target": "2-lfns_salon_3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-lfns_salon_2",
+      "name": "LFNS_SALON_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 1.6660205068961256,
+        "pitch": 0.01662499293366615,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4906508663198919,
+          "pitch": 0.5432941939585803,
+          "rotation": 3.141592653589793,
+          "target": "3-lfns_salon_4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-lfns_salon_3",
+      "name": "LFNS_SALON_3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.1006540698111138,
+        "pitch": -0.007012837253647319,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7118538896142041,
+          "pitch": 0.16172211784776458,
+          "rotation": 4.71238898038469,
+          "target": "3-lfns_salon_4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-lfns_salon_4",
+      "name": "LFNS_SALON_4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 1.7605549727060419,
+        "pitch": 0.02248797124646451,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.921964675226917,
+          "pitch": 0.1527920111038199,
+          "rotation": 4.71238898038469,
+          "target": "2-lfns_salon_3"
+        },
+        {
+          "yaw": 2.7917267067065117,
+          "pitch": 0.12448399249060316,
+          "rotation": 4.71238898038469,
+          "target": "0-lfns_salon_1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Lantre",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
